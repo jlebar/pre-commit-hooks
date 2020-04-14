2faca20c4cdc5e1941fe7f2a6cb60cbc6b86e1d6
Justin Lebar <jlebar@google.com>
2020-04-13 22:30:26 -0700

    Add clang-format pre-commit hook.

--- README.md
+++ README.md
@@ -6,5 +6,16 @@ In your `.pre-commit-hooks.yaml` file:
 repos:
 -   repo: https://github.com/CamusEnergy/pre-commit-hooks.git
     hooks:
+      -   id: do-not-submit
       -   id: bazel-buildifier
+      -   id: clang-format
 ```
+
+# Notes on clang-format rules
+
+The clang-format rules this repository provides are different from those in most
+other pre-commit repositories I've seen.
+
+Rather than using the clang-format binary you might have on your system, this
+repository fetches a binary at a fixed version.  This is important because
+clang-format's formatting changes between versions!
---
+++ clang_format.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+"""Runs git-clang-format, invoking a particular clang-format binary.
+
+Download the clang-format binary if necessary.
+
+Usage example:
+
+  clang_format.py 10.0.0 foo.cpp bar.h
+"""
+
+import glob
+import hashlib
+import os
+import platform
+import shutil
+import stat
+import subprocess
+import sys
+import tempfile
+import urllib.request
+
+from pathlib import Path
+
+# clang-format sha1s were retrieved at Chromium rev
+# 81cc23a856578b149a37dd109b147d8544f9cbd8.
+#
+# https://github.com/chromium/chromium/blob/master/buildtools/linux64/clang-format.sha1
+# https://github.com/chromium/chromium/blob/master/buildtools/mac/clang-format.sha1
+# https://github.com/chromium/chromium/blob/master/buildtools/win/clang-format.exe.sha1
+CLANG_FORMAT_SHAS = {
+    "Linux": "1baf0089e895c989a311b6a38ed94d0e8be4c0a7",
+    "Darwin": "62bde1baa7196ad9df969fc1f06b66360b1a927b",
+    "Windows": "d4afd4eba27022f5f6d518133aebde57281677c9",
+}
+
+
+def download_clang_format(sha: str, dest: Path):
+    download_path = (
+        f"https://commondatastorage.googleapis.com/chromium-clang-format/{sha}"
+    )
+    print("Downloading clang-format (~2mb)...")
+    with tempfile.TemporaryDirectory() as tempdir_name:
+        tempdir = Path(tempdir_name)
+        with urllib.request.urlopen(download_path) as download_file:
+            with tempdir.joinpath("clang-format").open("wb") as outfile:
+                shutil.copyfileobj(download_file, outfile)
+
+            st = os.stat(outfile.name)
+            os.chmod(outfile.name, st.st_mode | stat.S_IEXEC)
+
+            print(f"Moving downloaded clang-format to {dest.resolve()}")
+            Path(outfile.name).rename(dest)
+
+
+def check_hash(sha: str, file: Path):
+    d = hashlib.sha1()
+    with open(file, "rb") as f:
+        while True:
+            chunk = f.read(1024 * 64)
+            if not chunk:
+                break
+            d.update(chunk)
+
+    if d.hexdigest() != sha:
+        print(
+            f"FATAL: sha1sum mismatch on {file}.  Expected {sha}, but was {d.hexdigest()}",
+            file=sys.stderr,
+        )
+        print("Maybe the file is corrupted?  Try deleting it.")
+        sys.exit(1)
+
+
+def clang_format_path() -> Path:
+    """Gets the path of the relevant clang-format binary.
+
+  Downloads it if necessary.
+  """
+    try:
+        base_cachedir = Path(os.environ["XDG_CACHE_HOME"])
+    except KeyError:
+        base_cachedir = Path(os.environ["HOME"]).joinpath(".cache")
+
+    cachedir = base_cachedir.joinpath("pre-commit-jlebar")
+
+    if not cachedir.exists():
+        cachedir.mkdir()
+        with cachedir.joinpath("README").open("w") as f:
+            f.write(
+                """\
+This directory is maintained by jlebar's pre-commit hooks.
+Learn more: https://github.com/jlebar/pre-commit-hooks
+"""
+            )
+
+    clang_format_sha = CLANG_FORMAT_SHAS[platform.system()]
+    clang_format_file = cachedir.joinpath("clang-format-" + clang_format_sha)
+
+    if not clang_format_file.exists():
+        download_clang_format(clang_format_sha, clang_format_file)
+
+    check_hash(clang_format_sha, clang_format_file)
+    return clang_format_file
+
+
+def main():
+    this_dir = os.path.dirname(__file__)
+    git_cf_path = os.path.join(this_dir, "git-clang-format")
+    print('Formatting ' + ' '.join(sys.argv[1:]))
+    subprocess.run(
+        [git_cf_path, "-f", "--binary", str(clang_format_path())] + sys.argv[1:]
+    )
+
+
+if __name__ == "__main__":
+    main()
